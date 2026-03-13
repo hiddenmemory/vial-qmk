4930ca1353cb90e36660d8f5e5a06172062967d1
Chris Ross <chris@hiddenmemory.co.uk>
2026-03-13 16:30:56 +0000

    Remove an allocation when rendering out fonts

--- rust/qmk-rs/crates/keyboard/src/fonts.rs
+++ rust/qmk-rs/crates/keyboard/src/fonts.rs
@@ -1,4 +1,3 @@
-use alloc::vec::Vec;
 use include_image::*;
 
 use crate::{
@@ -9,35 +8,15 @@ use crate::{
 include_font!("./images/font.ttf", 140, "0123456789", LARGE_NUMBERS);
 include_font!("./images/font.ttf", 24, ASCII("→"), SMALL);
 include_font!("./images/font.ttf", 32, ASCII, LARGE);
-
-pub fn slices<const A: usize, const B: usize>(
-    font: &include_image::Font<A, B>,
-    value: &str,
-) -> Vec<(char, Rect, bool)> {
-    value
-        .chars()
-        .flat_map(|c| {
-            if c == ' ' {
-                Some((c, Rect::new(0, 0, font.space_width as u16, 0), false))
-            } else {
-                font.slice_for_char(c).map(|r| {
-                    (
-                        c,
-                        Rect::new(r.x as u16, r.y as u16, r.width as u16, r.height as u16),
-                        true,
-                    )
-                })
-            }
-        })
-        .collect()
-}
-
 pub fn width<const A: usize, const B: usize>(font: &include_image::Font<A, B>, value: &str) -> u16 {
-    slices(font, value)
-        .iter()
-        .map(|(_, r, _)| r.size.width + font.character_padding as u16)
-        .sum::<u16>()
-        - font.character_padding as u16
+    value.chars().fold(0, |total, c| {
+        let width = if c == ' ' {
+            font.space_width as u16
+        } else {
+            font.slice_for_char(c).map(|r| r.width as u16).unwrap_or(0)
+        };
+        width + font.character_padding as u16 + total
+    }) - font.character_padding as u16
 }
 
 pub fn render<const A: usize, const B: usize>(
@@ -52,23 +31,29 @@ pub fn render<const A: usize, const B: usize>(
     let (r, g, b) = fg.to_rgb8();
     let font_with_colour = font.with_colour(r, g, b);
 
-    for (_char, slice, render) in slices(font, value) {
+    for char in value.chars() {
+        let render = char != ' ';
+        let Some(include_image::Rect {
+            x,
+            y,
+            width,
+            height,
+        }) = font.slice_for_char(char)
+        else {
+            continue;
+        };
+
         if render {
             let position = Point::at(at.x + walking_x, at.y);
             display.render_image_slice(
                 position,
                 &font_with_colour as &dyn include_image::Image,
-                slice,
+                Rect::new(x as u16, y as u16, width as u16, height as u16),
                 bg,
             );
-
-            // display.stroke_rect(
-            //     Rect::new(position.x, position.y, slice.size.width, slice.size.height),
-            //     HSV_WHITE,
-            // );
         }
 
-        walking_x += slice.size.width + font.character_padding as u16;
+        walking_x += width as u16 + font.character_padding as u16;
     }
 }
 
